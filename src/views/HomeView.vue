ef424ad857a60c8479483ac417d54382c99bd7c4
mouadh-dev <mouadh.laa@gmail.com>
2022-11-25 23:26:42 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import navbar from '../components/navbar.vue'
+</script>
+
+<template>
+  <main>
+    <navbar/>
+    <TheWelcome />
+  </main>
+</template>
